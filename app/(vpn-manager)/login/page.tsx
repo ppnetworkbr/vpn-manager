db914c2d04da173e7c7da145cb199d0f36d7344e
Caik Pigosso <cpigosso@grupowedev.com.br>
2024-10-04 00:21:51 -0300

    Refatorar importação de página de gerenciamento de VPN, atualizar dependência do next-auth para a versão 5.0.0-beta.22 e adicionar redirecionamento para a página de login se o usuário não existir

--- app/(vpn-manager)/login/page.tsx
+++ app/(vpn-manager)/login/page.tsx
@@ -4,18 +4,22 @@ import { signIn } from 'next-auth/react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
-import { Button, TextField, Alert, Box } from '@mui/material'
+import { Button, TextField, Alert, Box, Typography } from '@mui/material'
 import { useRouter, useSearchParams } from 'next/navigation'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useServerAction } from 'zsa-react'
+import { countUsersServerAction } from '@/app/actions/countUsers'
 
 const loginSchema = z.object({
-  email: z.string().min(1, 'email é obrigatório'),
-  password: z.string().min(1, 'Password é obrigatório'),
+  email: z.string().min(1, 'Email é obrigatório'),
+  password: z.string().min(1, 'Senha é obrigatório'),
 })
 
 type LoginSchema = z.infer<typeof loginSchema>
 
 export default function LoginPage() {
+  const { execute } = useServerAction(countUsersServerAction, {})
+
   const {
     register,
     handleSubmit,
@@ -26,7 +30,7 @@ export default function LoginPage() {
   const router = useRouter()
   const [loginError, setLoginError] = useState<string | null>(null)
   const paramsRedirect = useSearchParams().get('returnUrl')
-  const onSubmit = async (data: LoginSchema) => {
+  async function onSubmit(data: LoginSchema) {
     setLoginError(null) // Reset login error
     const res = await signIn('credentials', {
       email: data.email,
@@ -40,39 +44,70 @@ export default function LoginPage() {
       router.push(paramsRedirect || '/')
     }
   }
-
+  async function existUsers() {
+    const [numberUsers] = await execute()
+    if (numberUsers === 0) {
+      router.push('/createFirstUser')
+    }
+  }
+  useEffect(() => {
+    existUsers()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
     <Box
-      component="form"
-      onSubmit={handleSubmit(onSubmit)}
-      noValidate
-      sx={{ maxWidth: 400, mx: 'auto', mt: 8 }}
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
     >
-      <TextField
-        label="Username"
-        {...register('email')}
-        error={!!errors.email}
-        helperText={errors.email?.message}
-        fullWidth
-        margin="normal"
-      />
-      <TextField
-        label="Password"
-        type="password"
-        {...register('password')}
-        error={!!errors.password}
-        helperText={errors.password?.message}
-        fullWidth
-        margin="normal"
-      />
-      {loginError && (
-        <Alert severity="error" sx={{ mb: 2 }}>
-          {loginError}
-        </Alert>
-      )}
-      <Button type="submit" variant="contained" color="primary" fullWidth>
-        Login
-      </Button>
+      <Box
+        component="form"
+        onSubmit={handleSubmit(onSubmit)}
+        noValidate
+        sx={{
+          maxWidth: 400,
+          mx: 'auto',
+          borderColor: 'primary.main',
+          borderWidth: 1,
+          boxShadow: 4,
+          borderRadius: 1,
+          p: 2,
+        }}
+      >
+        <Typography variant="subtitle2" align="center" gutterBottom>
+          Criei o primeiro usuário administrador
+        </Typography>
+        <TextField
+          label="Email"
+          {...register('email')}
+          error={!!errors.email}
+          helperText={errors.email?.message}
+          fullWidth
+          required
+          margin="normal"
+        />
+        <TextField
+          label="Senha"
+          required
+          type="password"
+          {...register('password')}
+          error={!!errors.password}
+          helperText={errors.password?.message}
+          fullWidth
+          margin="normal"
+        />
+        {loginError && (
+          <Alert severity="error" sx={{ mb: 2 }}>
+            {loginError}
+          </Alert>
+        )}
+        <Button type="submit" variant="contained" color="primary" fullWidth>
+          Login
+        </Button>
+      </Box>
     </Box>
   )
 }
