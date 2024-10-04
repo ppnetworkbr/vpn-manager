e5bf4848b86b45811b140ffa6693a83368d94dbb
Caik Pigosso <cpigosso@grupowedev.com.br>
2024-10-04 00:21:56 -0300

    Refatorar função countUsers para adicionar uma ação de servidor e contar a quantidade de usuários

--- app/actions/countUsers.ts
+++ app/actions/countUsers.ts
@@ -1,7 +1,14 @@
 import { findManyUsers } from '@/lib/actions/user.db.action'
+import { createServerAction } from 'zsa'
 
 export default async function countUsers(): Promise<number> {
   const users = await findManyUsers({})
 
   return users.length
 }
+
+export const countUsersServerAction = createServerAction().handler(async () => {
+  const users = await findManyUsers({})
+
+  return users.length
+})
